c9632e1c860d732c41821ce564227284789046e1
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-03-24 20:54:06 +0200

    new route for orders page

--- E-commerce/ecommerce/src/App.jsx
+++ E-commerce/ecommerce/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Routes>
     <Route index element={<HomePage />} />
     <Route path="checkout" element={<CheckoutPage />} />
+    <Route path="orders" element={<OrdersPage />} />
     </Routes>
     </>
   )
